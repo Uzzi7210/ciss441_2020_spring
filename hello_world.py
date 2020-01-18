a2cd96064ac13fae18b123cd36b4ba3cd45f1341
Uzzi7210 <59945403+Uzzi7210@users.noreply.github.com>
2020-01-18 13:59:25 -0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+my_name = "Ujjwal Bhattarai ( Uzzi )"
+#Some things to know abbout me.
+print ("Hello World, I am from Nepal and My name is " +  my_name)
+print("I love soccer and have played for muliple clubs as well as, i had gone for the tryouts for my U-14 Nepali National Team")
+print ("I love to sing and i wanna be a travel artist in the near future")
+print ("Throuh my IT degree i wanna work in a hospital, as well as own a business")
